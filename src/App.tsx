87b92c1f359195d750055d1dfa5673e6da770637
Ulisses Malanski <malanskiwork@gamil.com>
2023-07-06 19:02:09 -0300

    separete task Input and Output in components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import styles from './App.module.css';
 import './global.css';
 import { Header } from './components/Header';
-import { AddTask } from './components/addtask';
+import { AddTask } from './components/addTask';
 
 function App() {
 
--- src/components/addtask.tsx
+++ src/components/addtask.tsx
@@ -1,35 +1,25 @@
 import { useState } from 'react';
-import styles from './AddTask.module.css';
-
-interface Task {
-  content: string;
-  checked: boolean;
-}
+import styles from './addTask.module.css';
+import InfoTask from './InfoTask';
 
 export const AddTask = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<string[]>([]);
   const [newTask, setNewTask] = useState('');
-
-  const handleCheckboxChange = (index: number) => {
-    setTasks((prevTasks) =>
-      prevTasks.map((task, i) => (i === index ? { ...task, checked: !task.checked } : task))
-    );
-  };
 
   const handleAddTask = () => {
     if (newTask.trim() === '') return;
 
-    setTasks((prevTasks) => [...prevTasks, { content: newTask, checked: false }]);
+    setTasks((prevTasks) => [...prevTasks, newTask]);
     setNewTask('');
   };
 
   return (
-    <div className={styles.tasks}>
+    <div className={styles.addTask}>
       <h1>Todo</h1>
       <h2>Tasks</h2>
-      <label className={styles.taskContainer} htmlFor="input" title="Create a new Task">
+      <label className={styles.addTaskContainer} htmlFor="input" title="Create a new Task">
         <input
-          className={styles.checkInput}
+          className={styles.AddInput}
           type="text"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
@@ -39,29 +29,8 @@ export const AddTask = () => {
         </button>
       </label>
 
-      {tasks.map((task, index) => (
+      <InfoTask tasks={tasks} />
 
-        <label className={styles.taskContainer} key={index}>
-          <button className={styles.checkInput}
-            onClick={() => handleCheckboxChange(index)}>
-              <input
-                className={styles.checkBox}
-                type="checkbox"
-                checked={task.checked}
-                onChange={() => handleCheckboxChange(index)}
-              />
-
-            <span
-              style={{
-                textDecoration: task.checked ? 'line-through' : 'none',
-                background: task.checked ? '#323a3a' : 'none',
-              }}
-            >
-              &ensp;{task.content}
-            </span>
-          </button>
-        </label>
-      ))}
     </div>
   );
 };
